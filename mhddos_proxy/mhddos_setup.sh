4611ba991d61effa5ff0f1d0f7d1de5499125d95
lazyloadtest <lazyloadtest@example.com>
2022-04-17 21:34:59 +1000

    common docker setup

--- mhddos_proxy/mhddos_setup.sh
+++ mhddos_proxy/mhddos_setup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# need to modify runuser line if not on ec2 as variables does not parse
 ENDUSER=$(id -u -n 1000)
 GITPATH=/home/$ENDUSER/lazy-loadtest
-sudo yum install docker -y
-sudo usermod -aG docker $ENDUSER
-sudo systemctl enable docker.service docker.socket
-sudo systemctl restart docker
-chown -R $ENDUSER.$ENDUSER /home/$ENDUSER
 cp $GITPATH/mhddos_proxy/user-crontab /var/spool/cron/$ENDUSER
 chown $ENDUSER.$ENDUSER /var/spool/cron/$ENDUSER
 runuser -l $ENDUSER -c 'sh '$GITPATH'/mhddos_proxy/run_mhddos.sh'
