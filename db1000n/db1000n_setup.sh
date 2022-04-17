ecad899edc60c0c3a577cf6d2f9924835e029414
lazyloadtest <lazyloadtest@example.com>
2022-04-17 23:07:37 +1000

    fix setup

--- db1000n/db1000n_setup.sh
+++ db1000n/db1000n_setup.sh
@@ -5,5 +5,5 @@ cp $GITPATH/db1000n/user-crontab /var/spool/cron/$ENDUSER
 chown $ENDUSER.$ENDUSER /var/spool/cron/$ENDUSER
 cat $GITPATH/common/common-compose.yml $GITPATH/db1000n/db1000n-compose.yml >> /home/$ENDUSER/docker/docker-compose.yml
 chown -R $ENDUSER.$ENDUSER /home/$ENDUSER
-runuser -l $ENDUSER -c 'docker-compose -f /home/'$ENDUSER'/docker-compose.yml up -d'
+runuser -l $ENDUSER -c 'docker-compose -f /home/'$ENDUSER'/docker/docker-compose.yml up -d'
 /usr/bin/docker exec openvpn-client apk add curl
