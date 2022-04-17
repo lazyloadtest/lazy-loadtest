acfa8b9fa80a4f4793623288dd3e3290eaffa2c3
lazyloadtest <lazyloadtest@example.com>
2022-04-17 22:27:09 +1000

    fix formatting

--- db1000n/db1000n_setup.sh
+++ db1000n/db1000n_setup.sh
@@ -3,7 +3,7 @@ ENDUSER=$(id -u -n 1000)
 GITPATH=/home/$ENDUSER/lazy-loadtest
 cp $GITPATH/db1000n/user-crontab /var/spool/cron/$ENDUSER
 chown $ENDUSER.$ENDUSER /var/spool/cron/$ENDUSER
-cat '$GITPATH'/common/common-compose.yml '$GITPATH'/db1000n/db1000n-compose.yml >> /home/$ENDUSER/docker-compose.yml
+cat $GITPATH/common/common-compose.yml $GITPATH/db1000n/db1000n-compose.yml >> /home/$ENDUSER/docker-compose.yml
 chown -R $ENDUSER.$ENDUSER /home/$ENDUSER
 runuser -l $ENDUSER -c 'docker-compose -f /home/'$ENDUSER'/docker-compose.yml up -d'
 /usr/bin/docker exec openvpn-client apk add curl
--- disbalancer/disbalancer_setup.sh
+++ disbalancer/disbalancer_setup.sh
@@ -3,6 +3,6 @@ ENDUSER=$(id -u -n 1000)
 GITPATH=/home/$ENDUSER/lazy-loadtest
 cp $GITPATH/disbalancer/user-crontab /var/spool/cron/$ENDUSER
 chown $ENDUSER.$ENDUSER /var/spool/cron/$ENDUSER
-cat '$GITPATH'/common/common-compose.yml '$GITPATH'/disbalancer/disbalancer-compose.yml >> /home/$ENDUSER/docker-compose.yml
+cat $GITPATH/common/common-compose.yml $GITPATH/disbalancer/disbalancer-compose.yml >> /home/$ENDUSER/docker-compose.yml
 chown -R $ENDUSER.$ENDUSER /home/$ENDUSER
 /usr/bin/docker exec openvpn-client apk add curl
