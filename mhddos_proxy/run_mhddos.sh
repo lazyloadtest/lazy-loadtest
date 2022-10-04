de555d85eaac3608170e2e05df6bf0baaac5ea80
lazyloadtest <lazyloadtest@example.com>
2022-10-04 22:15:32 +1000

    remove depreciated --itarmy switch

--- mhddos_proxy/run_mhddos.sh
+++ mhddos_proxy/run_mhddos.sh
@@ -2,4 +2,4 @@
 THREADS=$(nproc) ; THREADS=$(expr $THREADS \* 2000)
 docker stop mhddos_proxy
 docker pull ghcr.io/porthole-ascend-cinnamon/mhddos_proxy
-docker run -d --name mhddos_proxy --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy --lang en -t $THREADS --itarmy
+docker run -d --name mhddos_proxy --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy --lang en -t $THREADS
